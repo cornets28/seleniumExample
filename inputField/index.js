629413979c7d9565ee4c2d7ca8ede01ac8ce5de1
CornetS28 <corsam28@gmail.com>
2020-01-31 15:29:10 -0500

    registration form with selinium

---
+++ inputField/index.js
@@ -0,0 +1,17 @@
+const { Builder, By } = require("selenium-webdriver");
+
+const driver = new Builder().forBrowser("chrome").build();
+
+async function simpleRegistration() {
+  try {
+    await driver.get(
+      "https://rori4.github.io/selenium-practice/#/pages/practice/simple-registration"
+    );
+    await driver.findElement(By.name("email")).sendKeys("samuel@noukod.com");
+    await driver.findElement(By.id("password")).sendKeys("12345");
+    await driver.findElement(By.name("submit")).click();
+  } catch (error) {
+    console.log(error);
+  }
+}
+simpleRegistration();
