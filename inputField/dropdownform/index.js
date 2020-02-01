4eeb7faed05261e98c3afdd799c39ee87b0f0e26
CornetS28 <corsam28@gmail.com>
2020-02-01 12:48:02 -0500

    dropdown menu form with selenium

---
+++ inputField/dropdownform/index.js
@@ -0,0 +1,49 @@
+const { Builder, By } = require("selenium-webdriver");
+
+const driver = new Builder().forBrowser("chrome").build();
+
+async function dropdownForm() {
+  try {
+    await driver.get(
+      "https://rori4.github.io/selenium-practice/#/pages/practice/dropdown-form"
+    );
+    const select1 = await driver.findElement(
+      By.xpath("//nb-select[@formcontrolname='select1']/button")
+    );
+    await select1.click();
+    await driver.findElement(By.xpath("//nb-option[@value='1']")).click();
+    await driver.findElement(By.xpath("//nb-option[@value='2']")).click();
+    await select1.click();
+
+    await driver
+      .findElement(By.xpath("//nb-select[@formcontrolname='select1']/button"))
+      .click();
+    await driver
+      .findElement(
+        By.xpath(
+          `//select[@formcontrolname='select2']/option[@value="0: 'volvo'"]`
+        )
+      )
+      .click();
+    await driver
+      .findElement(By.xpath(" //nb-select[@formcontrolname='select3']/button"))
+      .click();
+    await driver.findElement(By.xpath("//nb-option[@value='4']")).click();
+
+    const select4 = await driver.findElement(
+      By.xpath("//select[@formcontrolname='select4']")
+    );
+    await select4.click();
+    await driver
+      .findElement(
+        By.xpath("//select[@formcontrolname='select4']/option[@value='audi']")
+      )
+      .click();
+    await driver.findElement(By.xpath("//button[@name='submit']")).click();
+    await driver;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+dropdownForm();
